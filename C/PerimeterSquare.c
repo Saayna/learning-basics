9282a2365cc9a2655d4deb8aa6675971a3c81ac6
Saayna <148362266+Saayna@users.noreply.github.com>
2023-10-24 21:55:37 +0530

    Update PerimeterSquare.c
    Since its the perimeter of a square length and breadth won't come only length will be there.

--- C/PerimeterSquare.c
+++ C/PerimeterSquare.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-// Perimeter of Rectangle
+// Perimeter of Square
 int main() {
-    float l, b;
-    printf("Enter the Length of the Rectangle: ");
+    float l;
+    printf("Enter the Length of the Square: ");
     scanf("%f", &l);
-    printf("Enter the Breadth of the Rectangle: ");
-    scanf("%f", &b);
-    if(l<0 || b<0) {
-        printf("Invalid input as the length and breadth of a rectangle must be a non-negative values");
-        return 1;
+    if(l<0 ) {
+        printf("Invalid input as the length of a square must be a non-negative values");
     }
-    printf("The Perimeter of the Rectangle is %f", 2*(l+b));
+    else {
+    printf("The Perimeter of the Square is %f", 4*l);
+    }
+    return 0;
 }
