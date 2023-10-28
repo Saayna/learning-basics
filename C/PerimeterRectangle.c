0439803e3f219c9a23c074de7edc6a565c48bb26
Saayna <148362266+Saayna@users.noreply.github.com>
2023-10-28 20:30:52 +0530

    Update PerimeterRectangle.c
    In this program the title is perimeter of rectangle but it is calculating the perimeter of a square

--- C/PerimeterRectangle.c
+++ C/PerimeterRectangle.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-// Perimeter of Square
+// Perimeter of Rectangle
 int main() {
-    float a;
-    printf("Enter the Side of the Square: ");
-    scanf("%f", &a);
-    if(a<0){
-        printf("Invalid input side of a square must be a non-negative value");
+    float l, b;
+    printf("Enter the Length of the Rectangle: ");
+    scanf("%f", &l);
+    printf("Enter the Breadth of the Rectangle: ");
+    scanf("%f", &b);
+    if(l<0 || b<0) {
+        printf("Invalid input as the length and breadth of a rectangle must be a non-negative values");
         return 1;
     }
-    printf("The Perimeter of the Square is %f", 4*a);
+    printf("The Perimeter of the Rectangle is %f", 2*(l+b));
 }
