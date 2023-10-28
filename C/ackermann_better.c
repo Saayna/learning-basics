6f40eae7336416aea15bed47cba998c6074cb179
Saayna <148362266+Saayna@users.noreply.github.com>
2023-10-28 20:15:28 +0530

    Update ackermann_better.c
    A warning of implicit declaration of function is coming if we write void print before ack function .

--- C/ackermann_better.c
+++ C/ackermann_better.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+int ack(int m, int n){
+    if (m==0){
+        return n+1;
+    }
+    if (m!=0 && n==0){
+        return ack(m-1, 1);
+    }
+    if (m!=0 && n!=0){
+        return ack(m-1, ack(m, n-1));
+    }
+}
 void print(int a, int b){
     printf("\n|(0,0)");
     for(int n = 0; n<=a; n++){
@@ -17,17 +27,7 @@ void print(int a, int b){
     printf("\n");
 }
 
-int ack(int m, int n){
-    if (m==0){
-        return n+1;
-    }
-    if (m!=0 && n==0){
-        return ack(m-1, 1);
-    }
-    if (m!=0 && n!=0){
-        return ack(m-1, ack(m, n-1));
-    }
-}
+
 
 int main() {
     int a, b;
